0b99dd1bfab4c875590d059593dbc4c24f814ec0
ShowNaka <showta2704@gmail.com>
2020-10-11 15:16:09 +0900

    add

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to_active_hash :prefecture
-    belongs_to_active_hash :postage_type
-    belongs_to_active_hash :preparation_day
-    belongs_to_active_hash :postage_payer
-    belongs_to_active_hash :item_condition
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :postage_type
+  belongs_to_active_hash :preparation_day
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :item_condition
 
   belongs_to :user, optional: true
   belongs_to :brand, optional: true

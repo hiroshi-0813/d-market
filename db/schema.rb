09c3ed07d87bddc0644b8489223bcabcba6ec9b2
ShowNaka <showta2704@gmail.com>
2020-10-24 14:48:25 +0900

    change migration file

--- db/schema.rb
+++ db/schema.rb
@@ -123,21 +123,13 @@ ActiveRecord::Schema.define(version: 2020_10_10_140216) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
-    t.string "family_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "family_name_kana", null: false
-    t.date "birth_year", null: false
-    t.date "birth_month", null: false
-    t.text "introduction"
-    t.string "phone_number"
+    t.string "nickname", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end

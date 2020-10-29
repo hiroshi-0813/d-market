8cd07e37b3080902966e2e9d9a0d87d307181c5c
ShowNaka <showta2704@gmail.com>
2020-10-29 12:50:45 +0900

    add_commit

--- db/schema.rb
+++ db/schema.rb
@@ -85,20 +85,6 @@ ActiveRecord::Schema.define(version: 2020_05_12_024159) do
     t.index ["size_id"], name: "index_items_on_size_id"
   end
 
-  create_table "items_imgs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "url", null: false
-    t.bigint "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_items_on_category_id"
-    t.index ["condition_id"], name: "index_items_on_condition_id"
-    t.index ["delivery_date_id"], name: "index_items_on_delivery_date_id"
-    t.index ["delivery_fee_id"], name: "index_items_on_delivery_fee_id"
-    t.index ["delivery_way_id"], name: "index_items_on_delivery_way_id"
-    t.index ["prefecture_id"], name: "index_items_on_prefecture_id"
-    t.index ["size_id"], name: "index_items_on_size_id"
-  end
-
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
